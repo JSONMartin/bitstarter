4db8ea717c15d74ef0a6bef04ead58713d6e691f
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 20:58:14 +0000

    Cleaned up web.js, removed extra code and indented how I prefer

--- web.js
+++ web.js
@@ -2,18 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-
 app.get('/', function(request, response) 
-{
-    //response.send('Hello World 2!');
-   var fs = require('fs');
-   //var buf = new Buffer("Test buffer string!", "utf-8");
-   //    response.send ( buf.toString('utf-8') );
+	{
+	    var fs = require('fs');
 
-    var buf = fs.readFileSync("index.html");
-    response.send ( buf.toString('utf-8') );
-});
-
+	    var buf = fs.readFileSync("index.html");
+	    response.send ( buf.toString('utf-8') );
+	}
+);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
