6f343a07e14cdf2f666092fcc82952e8c05c7f20
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 20:21:46 +0000

    Tested buffer output with dummy string

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response)
 {
     //response.send('Hello World 2!');
    var fs = require('fs');
-   var buf = new Buffer(256);
+   var buf = new Buffer("Test buffer string!", "utf-8");
    
     response.send('Did not crash');
 
@@ -15,14 +15,14 @@ fs.readFileSync('index.html', function (err, data)
 		    {
 			if (err) throw err;
 			console.log(data);
-			buf = data;
+			//buf = data;
 		    });
     //buf.toString('utf-8');
     //response.send('Did not crash');
 });
 
 //data.toString('utf-8');
-buf.toString('utf-8');
+response.send( buf.toString('utf-8') );
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
