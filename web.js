5ba13a80f16a59a02fa46a1f19b7b52114d55f5f
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 20:16:53 +0000

    Reinstantiated buf variable, set buf = data

--- web.js
+++ web.js
@@ -7,21 +7,21 @@ app.get('/', function(request, response)
 {
     //response.send('Hello World 2!');
    var fs = require('fs');
-   var data = new Buffer(256);
+   var buf = new Buffer(256);
    
-    //response.send('Did not crash');
+    response.send('Did not crash');
 
 fs.readFileSync('index.html', function (err, data)
 		    {
 			if (err) throw err;
 			console.log(data);
-			//buf = data;
+			buf = data;
 		    });
     //buf.toString('utf-8');
     //response.send('Did not crash');
 });
 
-data.toString('utf-8');
+//data.toString('utf-8');
 
 
 var port = process.env.PORT || 5000;
