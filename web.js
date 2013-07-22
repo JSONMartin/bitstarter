d890aee02adc5ad147cc7138eae755ace5c74ac9
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 20:51:06 +0000

    console log

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response)
 {
     //response.send('Hello World 2!');
    var fs = require('fs');
-   var buf = new Buffer("Test buffer string!", "utf-8");
+   //var buf = new Buffer("Test buffer string!", "utf-8");
    
 //response.send('Did not crash');
 //    response.send ( buf.toString('utf-8') );
@@ -15,7 +15,7 @@ app.get('/', function(request, response)
 fs.readFileSync('index.html', function (err, data)
 		    {
 			if (err) throw err;
-			//console.log(data);
+			console.log(data);
 			//buf = data;
 			//response.send(data);
 		    });
@@ -25,9 +25,6 @@ fs.readFileSync('index.html', function (err, data)
 });
 
 //data.toString('utf-8');
-
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
