e62534c5b047dcff32c81d3007a666a28498b754
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 19:51:50 +0000

    Commented out fs.readFileSync to test if this is working

--- web.js
+++ web.js
@@ -8,7 +8,9 @@ app.get('/', function(request, response)
   //response.send('Hello World 2!');
    var fs = require('fs');
    //var buf = new Buffer(256);
-   fs.readFileSync('index.html', function (err, data)
+   
+/*
+fs.readFileSync('index.html', function (err, data)
 		    {
 			if (err) throw err;
 			console.log(data);
@@ -16,7 +18,7 @@ app.get('/', function(request, response)
 		    });
     //buf.toString('utf-8');
 });
-
+*/
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
