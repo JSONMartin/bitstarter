aa94f103d36923d605ff819d5e8b4e963d54ee4f
Ubuntu <ubuntu@ip-172-31-34-84.us-west-2.compute.internal>
2013-07-22 19:34:16 +0000

    Modified web.js to read index.html and print out with buffer

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+app.get('/', function(request, response) 
+{
+  //response.send('Hello World 2!');
+    var buffer = new Buffer(
+ fs.readFileSync('index.html', function (err, data)
+		    {
+			if (err) throw err;
+			console.log(data);
+		}), "utf-8");
+    buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
